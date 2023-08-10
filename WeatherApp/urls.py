540b24c8ed95add87394ff2c74ad9e4da5521424
AdityaNaiduGIT <asnjony@gmail.com>
2023-08-10 15:04:44 +0530

    try

--- WeatherApp/urls.py
+++ WeatherApp/urls.py
@@ -23,8 +23,8 @@ from .views import get_weather
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path('', TemplateView.as_view(template_name='index.html')),
-    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='index.html')),
+    # re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
     path('api/weather/', get_weather, name='get-weather'),
     
 ]
