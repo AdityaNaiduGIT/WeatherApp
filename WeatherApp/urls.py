33a2f51aab6bbaf23488ceca9ec85cd487e866a4
AdityaNaiduGIT <asnjony@gmail.com>
2023-08-10 15:00:31 +0530

    try

--- WeatherApp/urls.py
+++ WeatherApp/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import *
 from django.views.generic import TemplateView
 
 from .views import get_weather
@@ -23,7 +23,8 @@ from .views import get_weather
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html')),
+    # path('', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
     path('api/weather/', get_weather, name='get-weather'),
     
 ]
